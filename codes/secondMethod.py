5e07a2a93be85d09557b511661039f4e27e82e30
DamianoGiani <damiano.giani@gmail.com>
2022-08-17 09:48:10 +0200

    Update secondMethod.py

--- codes/secondMethod.py
+++ codes/secondMethod.py
@@ -2,7 +2,9 @@ import sys
 import os
 import cv2
 
-def secondMethod(path1,path2):  
+def secondMethod(path1,path2,pathRes):
+  a=(sys.argv)
+  
   images1 = [file for file in sorted(os.listdir(path1)) if file.endswith(('png'))]
   images2 = [file for file in sorted(os.listdir(path2)) if file.endswith(('png'))]
   count=0
@@ -12,14 +14,18 @@ def secondMethod(path1,path2):
       #imb[imb<30]=0
       imf[imf<25]=0
       imtot=imb+imf
-      cv2.imwrite('/content/results/MySecondMod/frame%d.png' % count, imtot)    
+      cv2.imwrite(pathRes % count, imtot)    
       count += 1
-  print('done')
+  print('second method finished')
 
 a=(sys.argv)  
 if a[1]=='001':
   path1='/content/results/Background/MyG_iter12000/background'
-else: path1='/content/results/Background/EGVSR_iter420000/background'
-path2='/content/results/Foreground/EGVSR_iter420000/foreground/'
-print(path1)
-secondMethod(path1,path2)      
+  pathRes='/content/results/MySecondMod/MyG_iter12000/frame%d.png'
+  path2='/content/results/Foreground/MyG_iter12000/foreground/'
+else: 
+  path1='/content/results/Background/EGVSR_iter420000/background'
+  pathRes='/content/results/MySecondMod/EGVSR_iter420000/frame%d.png'
+  path2='/content/results/Foreground/EGVSR_iter420000/foreground/'
+
+secondMethod(path1,path2,pathRes)   
